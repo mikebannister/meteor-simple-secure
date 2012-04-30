281586073bb023ebc8c0896b1aeb8cffcdb02fed
Mike Bannister <mikebannister@gmail.com>
2012-04-30 03:12:02 -0400

    fix github_url_2_examples

--- server/lib/github_url_2_examples.js
+++ server/lib/github_url_2_examples.js
@@ -5,8 +5,8 @@ GithubUrl2CodeExamples = {
     // A weird quick way to parse the examples out
     var rawExamples = code.split('// Example start: ');
     if (rawExamples) {
-      // get rid of the first 2 because it's source we don't care about
-      rawExamples.shift(); rawExamples.shift();
+      // get rid of the first one because it's source we don't care about
+      rawExamples.shift();
       return !rawExamples ? [] : _.map(rawExamples, function(rawExample) {
         var endLabelIndex = rawExample.indexOf('\n');
         var label = rawExample.substr(0, endLabelIndex);
